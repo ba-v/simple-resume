f807d90041d8969e90a54aa634855498649d73dd
ba-v <baharamirian@yahoo.com>
2019-10-02 21:13:23 +0330

    Updates

---
+++ precache-manifest.fb128e010e08ac3801725700af60d11c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74ae174f492f2d367ee7",
+    "url": "/simple-resume/static/css/main.f2999c27.chunk.css"
+  },
+  {
+    "revision": "74ae174f492f2d367ee7",
+    "url": "/simple-resume/static/js/main.c96734ea.chunk.js"
+  },
+  {
+    "revision": "45523f399806bb3b11d6",
+    "url": "/simple-resume/static/js/runtime~main.ef4bab71.js"
+  },
+  {
+    "revision": "35aafa8d6a1973773293",
+    "url": "/simple-resume/static/js/2.797bfaae.chunk.js"
+  },
+  {
+    "revision": "859dd4e66097a96d9744167bd6951790",
+    "url": "/simple-resume/index.html"
+  }
+];
